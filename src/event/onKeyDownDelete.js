f8a238fd1a783d9f2bc64494e9efec8c8821abf4
KimGyuMin <blackksj@naver.com>
2016-09-09 18:09:31 +0900

    삭제오류 확인
    삭제 기능시 오브젝트에 포커스가 잇으면 삭제되지 않아야함....

--- src/event/onKeyDownDelete.js
+++ src/event/onKeyDownDelete.js
@@ -2,8 +2,9 @@
 $(document).on("keydown", function(e) {
     if(e.keyCode == 46) {
         if($('.cursorDiv').length) {
-            selectedLastElement.remove();
-            clearCursor();
+            //포커스가 있을때는 삭제가 되지 않아아햠
+            //selectedLastElement.remove();
+            //clearCursor();
         } 
     }
 });
